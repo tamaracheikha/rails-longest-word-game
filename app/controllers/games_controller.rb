422696a081c37e81dd3d246f878b228d89218819
Tamara Cheikha <tamarasihamcheikha@gmail.com>
2021-04-16 11:48:59 +0100

    comit

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,62 +1,30 @@
-# require 'json'
-# require 'open-uri'
-
-# class GamesController < ApplicationController
-#   def new
-#     alphabet = ('a'..'z').to_a
-#     letters = []
-#     letters_size = 11
-#     i = 0
-#     while i < letters_size
-#       letters << alphabet.sample(1)
-#       i += 1
-#     end
-#     @letters = letters.flatten
-#   end
-
-#   def score
-#     @word = params[:word]
-#     url = "https://wagon-dictionary.herokuapp.com/#{@word}"
-#     response = JSON.parse(open(url).read)
-#     if response['found']
-#       if @word.chars
-
-#       end
-#     end
-#   end
-# end
-
 require "open-uri"
+
 class GamesController < ApplicationController
+  VOWELS = %w(A E I O U Y)
+
   def new
-    alphabet = ('a'..'z').to_a
-    letters_array = []
-    letters_size = 11
-    i = 0
-    while i < letters_size
-        letters_array << alphabet.sample(1)
-        i += 1
-    end
-    @letters = letters_array.flatten
+    @letters = Array.new(5) { VOWELS.sample }
+    @letters += Array.new(5) { (('A'..'Z').to_a - VOWELS).sample }
+    @letters.shuffle!
   end
-  def letter_check?(attempt, grid)
-    attempt_array = attempt.chars
-    return attempt_array.all? { |letter| attempt_array.count(letter) <= grid.count(letter) }
-  end
-  def check_english_word?(attempt)
-    url = "https://wagon-dictionary.herokuapp.com/#{attempt}"
-    word_serialized = open(url)
-    word = JSON.parse(word_serialized.read)
-    return word['found']
-  end
+
   def score
-    @attempt = params[:word]
-    @grid = params[:grid]
-    if letter_check?(@attempt, @grid) && check_english_word?(@attempt)
-      @final_score = @attempt.length
-    else
-      @final_score = 0
-    end
-    return @final_score
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
   end
 end
